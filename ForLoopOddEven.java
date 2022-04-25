5db2596db786418827b2ab726c5e661f0867de98
pareshpatel <pareshpatel75@gmail.com>
2022-04-25 11:31:17 +0100

    monday change

--- ForLoopOddEven.java
+++ ForLoopOddEven.java
@@ -3,21 +3,21 @@ package Loopweek;
 public class ForLoopOddEven {
     public static void main(String[] args) {
 
-       // create odd number using for loop 1-21
-        for (int i = 0; i <21; i++) {
-            if (i % 2 == 1) {
-                System.out.println(i);
+        // create odd number using for loop 1-21
+       for(int i=0; i<21; i++){
+           System.out.println(i);
+           i++;
+           //output will be even numbers 1-21
+                }
+       for (int i=1; i<22; i++){
+           System.out.println(i);
+           i++;
             }
-        }
-        // create even number using for loop 1-22
-        for(int i=0; i<22; i++){
-            if (i%2==0){
-                System.out.println(i);
-            }
+       // output will be odd numbers from 1-22
         }
     }
 
-}
+
 
 
 
